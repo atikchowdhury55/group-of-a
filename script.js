05f0784aa2ca3c8673cb8e43e7d6a20c767f77df
atikchowdhury55 <shikoacount25@gmail.com>
2025-02-19 12:14:07 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menuToggle").addEventListener("click", function () {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("-translate-x-full"); // Toggle the sidebar visibility
+});
